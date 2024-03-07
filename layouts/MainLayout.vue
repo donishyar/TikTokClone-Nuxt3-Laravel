323617b69841b4e4269d3352afb3a69848b8c346
donishyar <ahmadshahdonishyar@gmail.com>
2024-03-07 15:11:25 +0430

    working on sub header

--- layouts/MainLayout.vue
+++ layouts/MainLayout.vue
@@ -1,6 +1,16 @@
 <template>
   <TopNav />
-  <div></div>
+  <div
+    :class="route.fullPath === '/' ? 'max-w-[1140px]' : ''"
+    class="flex justify-between mx-auto w-full lg:px-2.5 px-0"
+  >
+    <div>
+      <SideNavMain />
+    </div>
+  </div>
 </template>
 
-<script></script>
+<script setup>
+import SideNavMain from "../components/SideNavMain.vue";
+const route = useRoute();
+</script>
