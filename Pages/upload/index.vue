c90333100651e68465a1576487d74c2eb0af7b98
donishyar <ahmadshahdonishyar@gmail.com>
2024-03-09 13:50:03 +0430

    working on upload section

---
+++ Pages/upload/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <UploadLayout>
+    <div
+      class="w-full mt-[80px] mb-[40px] bg-white shadow-lg rounded-md py-6 md:px-10 px-4"
+    >
+      <div>
+        <div class="text-[23px] font-semibold">Upload video</div>
+        <div class="text-gray-400 mt-1">Post a video to your account</div>
+      </div>
+    </div>
+  </UploadLayout>
+</template>
+
+<script setup>
+import UploadLayout from "/layouts/UploadLayout.vue";
+</script>
